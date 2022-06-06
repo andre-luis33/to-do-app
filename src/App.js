4e77cdbba463656a3e64bc43e9322ae3f7d06002
André Luís <andreluismoura22@gmail.com>
2022-06-06 10:55:09 -0300

    Add: hotjar sdk, testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { createBrowserHistory } from 'history'
+import { hotjar } from 'react-hotjar';
 import Router from './router'
 
 const history = createBrowserHistory();
 
+const hjid = 3005834 // hotjar id
+const hjsv = 6 // hotjar snippet version
+
+hotjar.initialize(hjid, hjsv);
+hotjar.identify('USER_ID', { userProperty: 'value' });
 
 const App = () => {
    return <>
